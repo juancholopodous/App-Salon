1d28f651124ff2728d281f08aca421d2f38a1e2d
Juancho Laptop <juanandresroth2@gmail.con>
2026-06-24 00:12:03 -0300

    inicio en Laptop

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codigoconjuan/app-salon_php_mvc_js_sass',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e6e102af72119a9bf5b2d7516e15eb13b0705728',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codigoconjuan/app-salon_php_mvc_js_sass' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e6e102af72119a9bf5b2d7516e15eb13b0705728',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
