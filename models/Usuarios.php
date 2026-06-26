b79824aa0577eac81c17fd8d5a10b9a337fc81f3
Juancho Laptop <juanandresroth2@gmail.con>
2026-06-25 21:12:15 -0300

    creación de modelo Usuarios y arreglo de validación con extención de ActiveRecord

---
+++ models/Usuarios.php
@@ -0,0 +1,30 @@
+<?php
+namespace Model;
+
+class Usuarios extends ActiveRecord {
+    // Base de Datos
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id','nombre','apellido','email','password','telefono','admin','confirmado','token']; //columnasDB es para normalizar los datos que se mentiene en memoria
+
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $telefono;
+    public $admin;
+    public $confirmado;
+    public $token;
+
+    public function __construct($args = []) {
+        $this->id = $args['id'] ?? null; // "??" Si no esta precente, será nulo.
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->telefono = $args['telefono'] ?? '';
+        $this->admin = $args['admin'] ?? null; // admin y confirmado solo acepta 2 valores posibles.
+        $this->confirmado = $args['confirmado'] ?? null;
+        $this->token = $args['token'] ?? '';
+    }
+}
