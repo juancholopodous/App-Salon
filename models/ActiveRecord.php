764d5d8f34dcdd85f25d56e2fb050882c7f88ef9
Juancho Laptop <juanandresroth2@gmail.con>
2026-06-29 16:28:01 -0300

    generación de método where para consultar por columna y valor

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -116,6 +116,13 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+    // Buscar "algo" usando Where mediante dos variables.
+    public static function Where($columna, $valor) {
+        $query = "SELECT * FROM " . static::$tabla  ." WHERE {$columna} = '{$valor}'";
+        $resultado = self::consultarSQL($query);
+        return array_shift( $resultado ) ;
+    }
+
     // Obtener Registros con cierta cantidad
     public static function get($limite) {
         $query = "SELECT * FROM " . static::$tabla . " LIMIT {$limite}";
